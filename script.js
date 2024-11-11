504dfd39d0a1e5285b1643d9e2947d51a0333b6c
crossedf3 <156528671+crossedf3@users.noreply.github.com>
2024-11-11 22:41:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }  // force scroll position to top after refresh
+
+  document.addEventListener('DOMContentLoaded', () => {
+    // Get references to the menu toggle button and navigation menu
+    const menuToggle = document.getElementById('navbar-drop');
+    const navMenu = document.getElementById('dropbutton');
+
+      // Add click event listener to the menu toggle button
+      menuToggle.addEventListener('click', () => {
+        // Toggle the 'active' class to show/hide the menu
+        navMenu.classList.toggle('active');
+    });
+
+      // Add a click event listener to the document to handle closing the menu
+      document.addEventListener('click', (event) => {
+        // Check if the click is outside both the menu toggle and the menu itself
+        if (!menuToggle.contains(event.target) && !navMenu.contains(event.target)) {
+            // Remove the 'active' class to hide the menu
+            console.log('Click outside menu');
+            navMenu.classList.remove('active');
+        }
+    });
+});
